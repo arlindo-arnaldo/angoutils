4293812b11351f956b5d83bec472198945a5597b
Arlindo <arlindoarnaldocassuende@gmail.com>
2024-06-10 15:33:41 +0100

    Execuntando o composer install

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit9fad56506132a248a067c0d213048322
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit9fad56506132a248a067c0d213048322', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit9fad56506132a248a067c0d213048322', 'loadClassLoader'));
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor/angoutils',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8bcc72a4c845c05828a5a292f2e5fa00797f7f17',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor/angoutils' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8bcc72a4c845c05828a5a292f2e5fa00797f7f17',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
